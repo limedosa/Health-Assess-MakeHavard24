6713bde8dbfe202a9d6e8ff116553b1bc1c1d293
Linda Dominguez <109569567+limedosa@users.noreply.github.com>
2024-02-03 17:32:49 -0500

    navbar update

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,8 +6,21 @@ bootstrap = Bootstrap4(app)
 
 
 @app.route('/')
-def home():
-    return render_template('base.html')
+def homePage():
+    return render_template('index.html', title="Home")
+
+@app.route("/newInputs")
+def newInputsPage():
+    return render_template("newInputs.html", title="New Inputs")
+
+@app.route("/records")
+def recordsPage():
+    return render_template("records.html", title="Records")
+
+@app.route("/settings")
+def settingsPage():
+    return render_template("settings.html", title="Settings")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
