62e7812dabba80a5dc47819e4c8ee3b9eab4cd41
Linda Dominguez <109569567+limedosa@users.noreply.github.com>
2024-02-03 22:24:51 -0500

    recipes added

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template, request
 from openai import OpenAI
 import os
 
-openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+OPENAI_API_KEY='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'
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
 # construct a prompt
-user_prompt = f"You are a health assess assistance. You have been given the the heartbeat data: 70 bmp. Base on this data and the record, provide the insights and advisor for the user."
+user_prompt = f"You are a health assessment assistant. Given the the heartbeat data: 70 bmp and the records, provide insights and advise for the user in the format: x beats per minute falls within (normal/not normal date), average heart rate for people in age range given, advice:"
 
 print("Prompting openai with:\n", user_prompt)
 
@@ -23,12 +24,30 @@ response_text = openai_response.choices[0].message.content
 print("OpenAI response:\n", response_text)
 
 
+######### for the healthy meal + ingredients + average cost
+
+recipe_prompt = f"Give me a healthy meal idea reccomendation along with the ingredients and average cost in USD."
+
+print("Prompting recipe with openai:\n", recipe_prompt, type(recipe_prompt))
+
+# call openai to get a response
+openai_response_recipe = openai_client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": recipe_prompt}]
+)
+
+# get the response text
+response_text_recipe = openai_response_recipe.choices[0].message.content
+
+
+print("OpenAI response for recipes:)\n", response_text_recipe)
+
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap4(app)
 
 @app.route('/')
 def homePage():
-    return render_template('index.html', title="Home", response_text = response_text)
+    return render_template('index.html', title="Home", response_text = response_text, response_text_recipe=response_text_recipe)
 
 
 @app.route("/newInputs", methods=['GET'])
