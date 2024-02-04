1a3066d45e28a7b776f3068009d5a29b27e6c3d2
Linda Dominguez <109569567+limedosa@users.noreply.github.com>
2024-02-04 09:44:38 -0500

    parsing

--- flaskTry/app.py
+++ flaskTry/app.py
@@ -5,6 +5,17 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request
+from openai import OpenAI
+import os
+import serial
+import time
+
+
+
+
+
 app = Flask(__name__)
 
 def generate_data():
@@ -37,6 +48,52 @@ def generate_data():
     df = pd.DataFrame(list(dictNums.items()), columns=['time', 'rate'])
     return df
 
+
+def dataParser(data):
+    """parses data in the form: 
+    {“value”: 70, “timestamp”: “2024-02-04T02:30:58.590377”}
+
+    Args:
+        data (dict): heartrate, timestamp
+
+    Returns:
+        _type_: _description_
+    """
+    listHeartRate = [num for num in data.keys()]
+    listTimeStamps = [time for time in data.values()]
+    return listHeartRate, listTimeStamps
+
+def averageHeartRate(listHeartRate):
+    """gives average heart rate given a list of heart rates
+    Args:
+        listHeartRate (list): _description_
+    Returns:
+        num: average
+    """
+    return sum(listHeartRate)/len(listHeartRate)
+
+testingRate = averageHeartRate([2,4,6,8,10])
+
+openai_client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+# construct a prompt
+user_prompt = f"You are a health assessment assistant. Given the the heartbeat data: {testingRate} bmp and the records, provide insights and advise for the user in the format: x beats per minute falls within (normal/not normal date), average heart rate for people in age range given, advice:"
+
+print("Promp:\n", user_prompt)
+
+# call openai to get a response
+openai_response = openai_client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": user_prompt}]
+)
+
+# get the response text
+responseInputAnalysis = openai_response.choices[0].message.content
+
+# print the response
+print("OpenAI response:\n", responseInputAnalysis)
+
+
 @app.route('/')
 def index():
     # Generate data
@@ -53,7 +110,7 @@ def index():
     plt.close()
 
     # Render the template with the graph
-    return render_template('index.html')
+    return render_template('index.html', responseInputAnalysis=responseInputAnalysis)
 
 if __name__ == '__main__':
     app.run(debug=True)
